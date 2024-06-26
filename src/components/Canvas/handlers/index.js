b79ab20325795d0b2f75747d43f8759f747923cd
liaojunhao <460200123@qq.com>
2024-06-26 22:20:37 +0800

    初始化项目

---
+++ src/components/Canvas/handlers/index.js
@@ -0,0 +1,1 @@
+export { default as useContainerHandler } from './useContainerHandler';
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable no-undef */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // 其他配置...
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // 设置 '@' 指向 'src' 目录
+      components: path.resolve(__dirname, 'src/components'), // 直接指向 'src/components' 目录
+      // 你可以根据需要继续添加更多别名
+    },
+  },
+});
